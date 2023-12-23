13610f3e5a03603431609bb42ae080e216ff728e
blinkEgor <barminov44@gmail.com>
2023-12-23 16:21:15 +0300

    first program hello world

---
+++ first_program/index.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <cstdlib>
+using namespace std;
+
+int main() {
+    system("chcp 1251>nul");
+    cout<<"Programming on the C++"<<endl;
+    system("pause>nul");
+    return 0;
+}
