974cee557df8fc3b981ca6ae006f3cd2d9ebe05d
blinkEgor <barminov44@gmail.com>
2024-01-06 18:19:40 +0300

    create program convert mile to km

--- first_program/index.cpp
+++ first_program/index.cpp
@@ -2,7 +2,15 @@
 using namespace std;
 
 int main() {
-    cout<<"Programming on the C++"<<endl;
+    const double km_in_mile = 1.609344;
+    double dist_mile, dist_km;
+
+    cout<<"Enter the distance in miles: ";
+    cin>>dist_mile;
+
+    dist_km = dist_mile * km_in_mile;
+    cout<<"Distance in km: "<<dist_km<<endl;
+
     system("pause>nul");
     return 0;
 }
