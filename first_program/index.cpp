46ac44fdd9bb65d33beb059ae0f8ffe754cc53d9
blinkEgor <barminov44@gmail.com>
2024-01-06 18:49:22 +0300

    km and m

--- first_program/index.cpp
+++ first_program/index.cpp
@@ -2,14 +2,24 @@
 using namespace std;
 
 int main() {
+    const int ft_in_mile = 5280;
     const double km_in_mile = 1.609344;
-    double dist_mile, dist_km;
+    int d_mile, d_ft, d_km, d_m;
 
-    cout<<"Enter the distance in miles: ";
-    cin>>dist_mile;
+    cout<<"Distance in miles and feet."<<endl;
+    cout<<"Miles: ";
+    cin>>d_mile;
+    cout<<"Feet: ";
+    cin>>d_ft;
 
-    dist_km = dist_mile * km_in_mile;
-    cout<<"Distance in km: "<<dist_km<<endl;
+    double dist_mile = d_mile + (double)d_ft / ft_in_mile;
+    double dist_km = dist_mile * km_in_mile;
+    d_km = (int)dist_km;
+    d_m = (int)((dist_km - d_km) * 1000);
+
+    cout<<"Distance in kilometers and meters."<<endl;
+    cout<<"Kilometers: "<<d_km<<endl;
+    cout<<"Meters: "<<d_m<<endl;
 
     system("pause>nul");
     return 0;
