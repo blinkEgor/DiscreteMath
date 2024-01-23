c61e235b7fdd4a1938365cddb1cb2af4d04d21e1
blinkEgor <barminov44@gmail.com>
2024-01-23 18:32:19 +0300

    add loop while

--- first_program/index.cpp
+++ first_program/index.cpp
@@ -1,21 +1,14 @@
 #include <iostream>
 using namespace std;
 
-double getDistMile() {
-    double dist;
-    cout<<"Indicate the distance in miles: ";
-    cin>>dist;
-    return dist;
-}
-
-double getDistKm(double dist) {
-    double km_in_mile=1.609344;
-    return dist*km_in_mile;
-}
-
 int main() {
-    double dist_mile=getDistMile();
-    cout<<"Distance in km: "<<getDistKm(dist_mile)<<endl;
+    int n=10,s=0,k=1;
+
+    while(k<=n){
+        s=s+k*k;
+        k++;
+    }
+    cout<<"Sum of square from 1 to "<<n<<": "<<s<<endl;
 
     system("pause>nul");
     return 0;
