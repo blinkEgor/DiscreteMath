12fdb55876aa01fa97e25ea5a3a07c70d8df0757
blinkEgor <barminov44@gmail.com>
2024-02-11 17:00:35 +0300

    program for calculating distances in kilometers and meters in miles and feet

--- first_program/index.cpp
+++ first_program/index.cpp
@@ -1,18 +1,23 @@
 #include <iostream>
 using namespace std;
 
-double getMile(double km){
-    const double km_in_one_mile=1.609344;
-    return km/km_in_one_mile;
-}
-
 int main() {
-    double km,mile;
+    int km,mile,m,ft;
+    const int ft_in_mile=5280;
+    const double km_in_mile=1.609344;
 
-    cout<<"Enter the distance in kilometers: ";
+    cout<<"Enter the distance in kilometers and meters."<<endl;
+    cout<<"kilometers: ";
     cin>>km;
-    mile=getMile(km);
-    cout<<km<<" kilometers = "<<mile<<" miles "<<endl;
+    cout<<"Meters: ";
+    cin>>m;
+
+    double dist_km=km+(double)m/1000;
+    double dist_mile=dist_km/km_in_mile;
+    mile=(int)dist_mile;
+    ft=(int)((dist_mile-mile)*ft_in_mile);
+
+    cout<<"Miles: "<<mile<<" Feets: "<<ft<<endl;
     
     return 0;
 }
