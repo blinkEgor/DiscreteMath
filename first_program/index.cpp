5c0514c10995d9154b80b9cd0e56b50229562beb
blinkEgor <barminov44@gmail.com>
2024-02-11 15:52:10 +0300

    Displaying Binomial Coefficients

--- first_program/index.cpp
+++ first_program/index.cpp
@@ -2,22 +2,28 @@
 using namespace std;
 
 int main() {
-    int n,k=2;
-    cout<<"Write the number: ";
+    int n,k=0;
+    cout<<"Enter a positive natural number: ";
     cin>>n;
-    int fib[n+1];
-    fib[0]=0;
-    fib[1]=1;
-    cout<<"Fibonacci sequence: "<<endl;
-    cout<<" "<<fib[0]<<" "<<fib[1];
-    while (k<n)
+    if (n>=0)
     {
-        fib[k]=fib[k-1]+fib[k-2];
-        cout<<" "<<fib[k];
-        k++;
+        int bnm[n+1];
+        cout<<"Displaying Binomial Coefficients: "<<endl;
+        while (k<n)
+        {
+            bnm[0]=1;
+            bnm[k+1]=bnm[k]*(n-k)/(k+1);
+            
+            cout<<" "<<bnm[k];
+            k++;
+        }
+        
+        cout<<" "<<bnm[k];
+    }
+    else
+    {
+        cout<<n<<" is not a positive natural number!";
     }
     
-    cout<<endl;
-    system("pause>nul");
     return 0;
 }
