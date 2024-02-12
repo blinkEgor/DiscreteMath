b1f573b0041c2d24b84a3b558366c88fdbfdd810
blinkEgor <barminov44@gmail.com>
2024-02-12 14:44:41 +0300

    Array with even natural numbers

--- first_program/index.cpp
+++ first_program/index.cpp
@@ -2,12 +2,20 @@
 using namespace std;
 
 int main() {
-    int n,s=0;
-
+    int n,k=0;
     cout<<"Enter the natural number: ";
     cin>>n;
-    s=n*n;
-    cout<<"Sum of odd natural numbers: "<<s;
+    int enn[n+1];
+    enn[0]=0;
+    cout<<"Array with even natural numbers: "<<endl;
+    cout<<"[";
+    while (k<n)
+    {
+        enn[k+1]=enn[k]+2;
+        cout<<" "<<enn[k+1];
+        k++;
+    }
+    cout<<" ]"<<endl;
     
     return 0;
 }
