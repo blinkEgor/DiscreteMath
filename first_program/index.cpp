f7bb26bdf97b3a9748048503833bf90ef2033e92
blinkEgor <barminov44@gmail.com>
2024-02-12 13:14:57 +0300

    Speed conversion program m/s to km/h

--- first_program/index.cpp
+++ first_program/index.cpp
@@ -1,20 +1,20 @@
 #include <iostream>
 using namespace std;
 
-double getM_s(double km_h){
-    double m_s=km_h/3.6;
-    int ceil=m_s*100;
-    m_s=(double)ceil/100;
-    return m_s;
+double getKm_h(double m_s){
+    double km_h=m_s*3.6;
+    int ceil=km_h*100;
+    km_h=(double)ceil/100;
+    return km_h;
 }
 
 int main() {
     double km_h,m_s;
 
-    cout<<"Enter speed in km/h: ";
-    cin>>km_h;
-    m_s=getM_s(km_h);
-    cout<<"m/s: "<<m_s;
+    cout<<"Enter speed in m/s: ";
+    cin>>m_s;
+    km_h=getKm_h(m_s);
+    cout<<"km/h: "<<km_h;
     
     return 0;
 }
