e1e6f2f2ae72431c1b106e34d504d13eac81adbf
blinkEgor <barminov44@gmail.com>
2024-02-12 11:38:00 +0300

    Program for calculating distances from fathoms to meters

--- first_program/index.cpp
+++ first_program/index.cpp
@@ -2,22 +2,14 @@
 using namespace std;
 
 int main() {
-    int km,mile,m,ft;
-    const int ft_in_mile=5280;
-    const double km_in_mile=1.609344;
+    double fathoms,meters;
+    const double in_fathoms_of_meters=2.16;
 
-    cout<<"Enter the distance in kilometers and meters."<<endl;
-    cout<<"kilometers: ";
-    cin>>km;
-    cout<<"Meters: ";
-    cin>>m;
+    cout<<"Enter distance in fathoms: ";
+    cin>>fathoms;
 
-    double dist_km=km+(double)m/1000;
-    double dist_mile=dist_km/km_in_mile;
-    mile=(int)dist_mile;
-    ft=(int)((dist_mile-mile)*ft_in_mile);
-
-    cout<<"Miles: "<<mile<<" Feets: "<<ft<<endl;
+    meters=fathoms*in_fathoms_of_meters;
+    cout<<"Meters: "<<meters;
     
     return 0;
 }
