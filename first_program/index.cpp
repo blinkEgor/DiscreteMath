a1bbe7d1ad7e91c8f47fa71f1818a67e1db27cfe
blinkEgor <barminov44@gmail.com>
2024-02-12 13:43:54 +0300

    Sum of natural numbers

--- first_program/index.cpp
+++ first_program/index.cpp
@@ -1,20 +1,13 @@
 #include <iostream>
 using namespace std;
 
-double getKm_h(double m_s){
-    double km_h=m_s*3.6;
-    int ceil=km_h*100;
-    km_h=(double)ceil/100;
-    return km_h;
-}
-
 int main() {
-    double km_h,m_s;
+    int n,s=0;
 
-    cout<<"Enter speed in m/s: ";
-    cin>>m_s;
-    km_h=getKm_h(m_s);
-    cout<<"km/h: "<<km_h;
+    cout<<"Enter the natural number: ";
+    cin>>n;
+    s=(n*(n+1))/2;
+    cout<<"Sum of natural numbers: "<<s;
     
     return 0;
 }
