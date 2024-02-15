672207a41ed067ff1c101ef36505ee8148fef17f
blinkEgor <barminov44@gmail.com>
2024-02-15 18:54:05 +0300

    write an array program with Fibonacci numbers

--- first_program/index.cpp
+++ first_program/index.cpp
@@ -2,24 +2,24 @@
 using namespace std;
 
 int main() {
-    int n,k=0;
+    int n,k=2;
     cout<<"Enter the natural number: ";
     cin>>n;
-    int pot[n+1];
-    cout<<"Array with powers of two: "<<endl;
+    int fib[n+1];
+    fib[0]=1;
+    fib[1]=1;
+    cout<<"Array with Fibonacci numbers: "<<endl;
     cout<<"[";
+    if(n<2) {
+        cout<<" "<<fib[0];
+    }
+    if(n>=2) {
+        cout<<" "<<fib[0]<<" "<<fib[1];
+    }
     while (k<n)
     {
-        pot[k]=2;
-        int i=1;
-        while(i<k){
-            pot[k]*=2;   
-            i++;
-        }
-        if(k==0){
-            pot[k]=1;
-        }
-        cout<<" "<<pot[k];
+        fib[k]=fib[k-1]+fib[k-2];
+        cout<<" "<<fib[k];
         k++;
     }
     cout<<" ]"<<endl;
