abf0386bed0983bcf825d0dae7d541595c34f09f
blinkEgor <barminov44@gmail.com>
2024-02-20 15:31:47 +0300

    several commands in a block 'for'

--- first_program/index.cpp
+++ first_program/index.cpp
@@ -2,12 +2,10 @@
 using namespace std;
 
 int main() {
-    int n,s=0,k;
+    int n,s,k;
     cout<<"Enter an upper limit for the amount: ";
     cin>>n;
-    for(k=1;k<=n;k++){
-        s+=k*k;
-    }
+    for(k=1,s=0;k<=n;s+=k*k,k++);
     cout<<"Sum of squares of numbers from 1 to "<<n<<": "<<s<<endl;
 
     system("pause>nul");
