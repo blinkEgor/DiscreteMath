1d193fd5586caaa318de92011a352f02e76ecfc8
blinkEgor <barminov44@gmail.com>
2024-02-20 15:38:13 +0300

    the third and first blocks are empty

--- first_program/index.cpp
+++ first_program/index.cpp
@@ -2,10 +2,13 @@
 using namespace std;
 
 int main() {
-    int n,s,k;
+    int n,s=0,k=1;
     cout<<"Enter an upper limit for the amount: ";
     cin>>n;
-    for(k=1,s=0;k<=n;s+=k*k,k++);
+    for(;k<=n;){
+        s+=k*k;
+        k++;
+    }
     cout<<"Sum of squares of numbers from 1 to "<<n<<": "<<s<<endl;
 
     system("pause>nul");
