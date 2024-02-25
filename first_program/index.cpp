90b2988d4d38b8d082ff2fdb09e57aa32daca267
blinkEgor <barminov44@gmail.com>
2024-02-25 14:47:47 +0300

    nested conditional statements

--- first_program/index.cpp
+++ first_program/index.cpp
@@ -5,24 +5,24 @@ using namespace std;
 int main() {
     int num;
     srand(2);
-    for(int k=1;k<=10;k++){
-        num=2+rand()%7;
-        switch(num){
-            case 3:
-            case 6:
-                cout<<num<<": the number is divisible by three"<<endl;
-                break;
-            case 2:
-            case 4:
-            case 8:
-                cout<<num<<": power of two"<<endl;
-                break;
-            case 5:
-                cout<<num<<": five"<<endl;
-                break;
-            case 7:
-                cout<<num<<": seven"<<endl;
-                break;
+    for(int k=1;k<=5;k++){
+        cout<<"Pleace, enter numbers from 1 to 3: ";
+        cin>>num;
+        if(num==1){
+            cout<<"That's one"<<endl;
+        }
+        else{
+            if(num==2){
+                cout<<"That's two"<<endl;
+            }
+            else{
+                if(num==3){
+                    cout<<"That's three"<<endl;
+                }
+                else{
+                    cout<<"I don't know that number"<<endl;
+                }
+            }
         }
     }
 
