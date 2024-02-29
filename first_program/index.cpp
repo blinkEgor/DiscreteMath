f0bfe1d7ea4f1126438321e70686090c12deea31
blinkEgor <barminov44@gmail.com>
2024-02-29 10:15:28 +0300

    sum of even natural numbers

--- first_program/index.cpp
+++ first_program/index.cpp
@@ -3,18 +3,22 @@
 using namespace std;
 
 int main() {
-    int s=0,n,p=-1;
+    int s=0,n,p=-2,i=0;
 
-    cout<<"Please enter the number of odd natural numbers: ";
+    cout<<"Please enter the number of even natural numbers: ";
     cin>>n;
 
-    for (int i = 0; i < n; i++)
+    do
     {
         p+=2;
         s+=p;
-    }
+        i++;
+        // cout<<"p = "<<p;
+        // cout<<"; s = "<<s<<endl;
+    } while (i<n);
     
-    cout<<"Sum of odd natural numbers from 1 to "<<p<<": "<<s<<endl;
+    
+    cout<<"Sum of even natural numbers from 0 to "<<p<<": "<<s<<endl;
        
     system("pause>nul");
     return 0;
