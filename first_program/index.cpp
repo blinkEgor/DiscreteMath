7579f68c0ca7096cb87f1a77b68daf8ae4d147a9
blinkEgor <barminov44@gmail.com>
2024-03-04 20:06:58 +0300

    reverse array filter

--- first_program/index.cpp
+++ first_program/index.cpp
@@ -7,31 +7,25 @@ int main() {
     int n=10;
     srand(2);
     int arr[n];
-    int i,j,k,s;
+    int i,k,s;
 
     cout<<"An array of random numbers."<<endl;
 
-    cout<<"Before sorting:\n|";
+    cout<<"Array:\n|";
     for (k = 0; k < n; k++)
     {
         arr[k]=rand()%10;
         cout<<arr[k]<<" | ";
     }
     
-    for (i = 1; i <= n-1; i++)
+    for (i = 1; i <= n/2; i++)
     {
-        for (j = 0; j < n-i; j++)
-        {
-            if (arr[j]<arr[j+1])
-            {
-                s=arr[j+1];
-                arr[j+1]=arr[j];
-                arr[j]=s;
-            }        
-        }
+        s=arr[i-1];
+        arr[i-1]=arr[n-i];
+        arr[n-i]=s;
     }
     
-    cout<<"\nAfter sorting:\n|";
+    cout<<"\nReverse array:\n|";
     for (k = 0; k < n; k++)
     {
         cout<<arr[k]<<" | ";
