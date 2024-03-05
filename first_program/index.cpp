e43973e27e7dbd38304b92eaf0435a34e246bad4
blinkEgor <barminov44@gmail.com>
2024-03-05 13:15:29 +0300

    array of random numbers sorted in ascending order, odd first then even

--- first_program/index.cpp
+++ first_program/index.cpp
@@ -7,7 +7,7 @@ int main() {
     int n=10;
     srand(2);
     int arr[n];
-    int i,k,s;
+    int i,k,j,s;
 
     cout<<"An array of random numbers."<<endl;
 
@@ -18,14 +18,33 @@ int main() {
         cout<<arr[k]<<" | ";
     }
     
-    for (i = 1; i <= n/2; i++)
+    for (i = 1; i < n; i++)
     {
-        s=arr[i-1];
-        arr[i-1]=arr[n-i];
-        arr[n-i]=s;
+        for (j = 0; j < n-i; j++)
+        {
+            if (arr[j]>arr[j+1])
+            {
+                s=arr[j+1];
+                arr[j+1]=arr[j];
+                arr[j]=s;
+            }
+        }
     }
     
-    cout<<"\nReverse array:\n|";
+    for (i = 1; i < n; i++)
+    {
+        for (j = 0; j < n; j++)
+        {
+            if ((arr[j]%2==0)&&(arr[j+1]%2!=0))
+            {
+                s=arr[j+1];
+                arr[j+1]=arr[j];
+                arr[j]=s;
+            }
+        }
+    }
+    
+    cout<<"\nSorted array:\n|";
     for (k = 0; k < n; k++)
     {
         cout<<arr[k]<<" | ";
