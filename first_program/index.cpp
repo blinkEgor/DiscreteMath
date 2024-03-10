eb13870067cc8cf881f5c4bc127f240f20921ed8
blinkEgor <barminov44@gmail.com>
2024-03-10 10:04:29 +0300

    two-dimensional arrays v1.2

--- first_program/index.cpp
+++ first_program/index.cpp
@@ -1,24 +1,48 @@
 #include <iostream>
+#include <cstdio>
 using namespace std;
 
-int main() {
-    srand(2);
-    const int w=9;
-    const int h=5;
-    char Lts[h][w];
+const int n=3;
 
-    for (int i = 0; i < h; i++)
+void show(int M[n][n])
+{
+    for (int i = 0; i < n; i++)
     {
-        cout<<"| ";
-        for (int j = 0; j < w; j++)
+        for (int j = 0; j < n; j++)
         {
-            Lts[i][j]='A'+rand()%25;
-            cout<<Lts[i][j]<<" | ";
+            printf("%4d",M[i][j]);
         }
-        cout<<endl;
+        printf("\n");
     }
-    
+}
 
+int main()
+{
+    int A[n][n]={{1,-2,1},{2,0,-1},{2,3,-1}};
+    printf("Matrix A:\n");
+    show(A);
+
+    int B[n][n]={{2,1,-1},{1,3,1},{-2,1,4}};
+    printf("Matrix B:\n");
+    show(B);
+
+    int C[n][n];
+
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            C[i][j]=0;
+            for (int k = 0; k < n; k++)
+            {
+                C[i][j]+=A[i][k]*B[k][j];
+            }
+        }
+    }
+
+    printf("Matrix C=A*B:\n");
+    show(C);
+    
     system("pause>nul");
     return 0;
 }
