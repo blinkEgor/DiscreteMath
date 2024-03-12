02b4f939526a5d4a3ee5716c5f00c706df1aa756
blinkEgor <barminov44@gmail.com>
2024-03-12 11:06:11 +0300

    Sum of pairwise products of an array

--- first_program/index.cpp
+++ first_program/index.cpp
@@ -4,14 +4,33 @@ using namespace std;
 
 int main()
 {
-    const char*str[3]={{"red"},{"yellow"},{"green"}};
+    const int n=10;
+    int s=0;
+    int arr_one[n];
+    int arr_two[n];
 
-    for (int i = 0; i < 3; i++)
+    srand(2);
+
+    cout<<endl<<"Array one:\n|";
+    for (int i = 0; i < n; i++)
     {
-        cout<<str[i]<<endl;
+        arr_one[i]=rand()%10;
+        cout<<arr_one[i]<<" | ";
     }
 
-    cout<<str[2][0]<<str[2][1]<<str[2][2]<<str[1][0]<<endl;
+    cout<<endl<<"Array two:\n|";
+    for (int i = 0; i < n; i++)
+    {
+        arr_two[i]=rand()%10;
+        cout<<arr_two[i]<<" | ";
+    }
+
+    cout<<endl<<"Sum of pairwise products of an array: ";
+    for (int i = 0; i < n; i++)
+    {
+        s+=arr_one[i]*arr_two[i];
+    }
+    cout<<s<<endl;
     
     system("pause>nul");
     return 0;
