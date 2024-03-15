612b076ac084a119fc43ba7a56c0033278b60314
blinkEgor <barminov44@gmail.com>
2024-03-15 11:38:08 +0300

    largest (smallest) element

--- first_program/index.cpp
+++ first_program/index.cpp
@@ -7,12 +7,18 @@ int main()
 {
     srand(2);
     const int n=5;
-    int s=0;
+
+    int l,s;
+    int**p_l=new int*[n];
+    int**p_s=new int*[n];
+
     int**a=new int*[n];
 
     for (int i = 0; i < n; i++)
     {
         a[i]=new int[n];
+        p_l[i]=new int[n];
+        p_s[i]=new int[n];
     }
     
     cout<<"Matrix:";
@@ -26,22 +32,56 @@ int main()
         }
     }
 
+    l=a[0][0];
     for (int i = 0; i < n; i++)
     {
         for (int j = 0; j < n; j++)
         {
-            if (i == j || j==n-i-1)
-				s += a[i][j];
+            if (l<a[i][j])
+                l=a[i][j];
         }
     }
-
-    cout<<"\nSum = "<<s<<endl;
-
+    cout<<"\nLargest: "<<l<<endl;
+    cout<<"Position:";
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            if (l==a[i][j])
+                cout<<" ["<<i<<"]"<<"["<<j<<"]";
+        }    
+    }
+    
+    s=a[0][0];
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            if (s>a[i][j])
+                s=a[i][j];
+        }
+    }
+    cout<<"\nSmallest: "<<s<<endl;
+    cout<<"Position:";
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            if (s==a[i][j])
+                cout<<" ["<<i<<"]"<<"["<<j<<"]";
+        }    
+    }
+    cout<<endl;
+    
     for (int i = 0; i < n; i++)
     {
         delete[]a[i];
+        delete[]p_l[i];
+        delete[]p_s[i];
     }
     delete[]a;
+    delete[]p_l;
+    delete[]p_s;
     
     system("pause>nul");
     return 0;
