e17391a05916415b9d726e1882397b950ab86dc0
blinkEgor <barminov44@gmail.com>
2024-03-19 21:41:32 +0300

    product of rectangular matrices

--- first_program/index.cpp
+++ first_program/index.cpp
@@ -1,32 +1,78 @@
 #include <iostream>
-#include <cstdio>
-#include <cmath>
+#include <cstdlib>
 using namespace std;
 
 int main()
 {
-    const int n=5;
-    int mat[n][n];
+    const int m=5;
+    const int n=8;
+    const int l=3;
 
-    cout<<"Matrix:\n| ";
-    for (int i = 0; i < n; i++)
+    int**a=new int*[m];
+    int**b=new int*[n];
+    int**c=new int*[m];
+
+    for(int i=0;i<m;i++)
+        a[i]=new int[n];
+    for(int i=0;i<n;i++)
+        b[i]=new int[l];
+    for(int i=0;i<m;i++)
+        c[i]=new int[l];
+
+    srand(2);
+
+    for(int i=0;i<m;i++)
     {
-        for (int j = 0; j < n; j++)
-        {
-            if((i==j)||
-                (i+j==n-1))
-            {
-                mat[i][j]=1;
-            }
-            else
-                mat[i][j]=0;
-            cout<<mat[i][j]<<" | ";
-        }
-        if(i==n-1)
-            cout<<endl;
-        else
-            cout<<"\n| ";
+        for(int j=0;j<n;j++)
+            a[i][j]=rand()%10;
     }
+    for(int i=0;i<n;i++)
+    {
+        for(int j=0;j<l;j++)
+            b[i][j]=rand()%10;
+    }
+
+    for(int i=0;i<m;i++)
+    {
+        for(int j=0;j<l;j++)
+        {
+            c[i][j]=0;
+            for(int k=0;k<n;k++)
+                c[i][j]+=a[i][k]*b[k][j];
+        }
+    }
+
+    cout<<"Matrix A:";
+    for(int i=0;i<m;i++)
+    {
+        cout<<"\n| ";
+        for(int j=0;j<n;j++)
+            cout<<a[i][j]<<" | ";
+    }
+    cout<<"\nMatrix B:";
+    for(int i=0;i<n;i++)
+    {
+        cout<<"\n| ";
+        for(int j=0;j<l;j++)
+            cout<<b[i][j]<<" | ";
+    }
+    cout<<"\nMatrix C:";
+    for(int i=0;i<m;i++)
+    {
+        cout<<"\n| ";
+        for(int j=0;j<l;j++)
+            cout<<c[i][j]<<" | ";
+    }
+
+    for(int i=0;i<m;i++)
+        delete[]a[i];
+    for(int i=0;i<n;i++)
+        delete[]b[i];
+    for(int i=0;i<m;i++)
+        delete[]c[i];
+    delete[]a;
+    delete[]b;
+    delete[]c;
     
     system("pause>nul");
     return 0;
